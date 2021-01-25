0d4629aa43cf565ab9a865527ae7c9a86e827c57
ale <ale@snoozed.dev>
2021-01-25 20:20:57 -0300

    hacer que culo anuncie su culo

--- Assets/Scripts/Culo.cs
+++ Assets/Scripts/Culo.cs
@@ -7,7 +7,7 @@ public class Culo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("El gameobject llamado" + gameObject.name + " tiene culo.");
     }
 
     // Update is called once per frame
