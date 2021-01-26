a975f1d4e14bbd2d28cc50c93d9cbde65d96b01f
ale <ale@snoozed.dev>
2021-01-25 22:49:10 -0300

    add player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Player : MonoBehaviour
+{
+    float moveSpeed = 7f;
+    float mouseSensitivity = 7f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        CameraControl();
+
+        float moveX = Input.GetAxis("Horizontal") * (moveSpeed / 100);
+        float moveY = Input.GetAxis("Vertical") * (moveSpeed / 100);
+        transform.Translate(new Vector3(moveX,0,moveY));
+    }
+
+    void CameraControl()
+    {
+        // MIRAR COSTADOS
+        float lookX = Input.GetAxis("Look-X") * mouseSensitivity;
+        float lookY = Input.GetAxis("Look-Y") * mouseSensitivity;
+        transform.Rotate(Vector3.up * lookX);
+        GetComponentInChildren<Camera>().transform.Rotate(-Vector3.right * lookY);
+    }
+
+    void MetaControls()
+    {
+        // SALIR DEL JUEGITO
+        if (Input.GetButton("Cancel"))
+        {
+            EditorApplication.ExitPlaymode();
+        }
+    }
+}
