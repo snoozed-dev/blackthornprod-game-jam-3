44343f93d52e70cbe95dd7cbb1716473ada53c51
ale <ale@snoozed.dev>
2021-01-25 21:56:36 -0300

    3D

--- Assets/Scripts/Culo.cs
+++ Assets/Scripts/Culo.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class Culo : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         print("El gameobject llamado" + gameObject.name + " tiene culo.");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
