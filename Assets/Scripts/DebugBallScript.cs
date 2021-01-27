1fff54cbc828d44f885043d4bb9b08e574a756e6
ale <ale@snoozed.dev>
2021-01-26 22:31:38 -0300

    bl

---
+++ Assets/Scripts/DebugBallScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugBallScript : MonoBehaviour
+{
+    public float timeout = 8;
+
+    void Update()
+    {
+        timeout -= Time.deltaTime;
+        if (timeout <= 0) Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,25 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEditor;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 4f;
+    public float moveSpeed = 8f;
     public float mouseSensitivity = 1000f;
     public float verticalClamp = 80f;
-    public float jumpForce = 320f;
+    public float jumpForce = 7f;
 
+    public float IS = 1000;
+    public float ISDecreaseRate = 10;
+    public float ISDecreaseShootAmount = 50;
+
+    public Text ISIndicator;
+
+    public float verticalVelocity = 0f;
+    public float gravity = 1.5f;
     float verticalLookRotation;
     Transform cameraTransform;
-    Rigidbody rigidbody;
-    bool isGrounded = false;
+    CharacterController characterController;
+    bool canSecondJump;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         cameraTransform = GetComponentInChildren<Camera>().transform;
-        rigidbody = GetComponent<Rigidbody>();
+        ISIndicator = GetComponentInChildren<Text>();
+        characterController = GetComponent<CharacterController>();
     }
 
     void Update()
@@ -27,27 +37,58 @@ public class Player : MonoBehaviour
         CameraControl();
         MetaControls();
         MovementControl();
-        isGrounded = Physics.Raycast(new Ray(transform.position + Vector3.up * .2f, Vector3.down), .5f);
+        ShootControl();
+
+        ISControl();
     }
 
     void FixedUpdate()
     {
-        FixedMovementControl();    
+        FixedMovementControl();
+    }
+
+    void ISControl()
+    {
+        IS -= Time.deltaTime * ISDecreaseRate;
+        ISIndicator.text = Mathf.Round(IS).ToString();
+    }
+
+    void ShootControl()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            Physics.Raycast(new Ray(cameraTransform.position, cameraTransform.forward), out hit);
+            if (hit.collider)
+            {
+
+            }
+        }
+
     }
     void MovementControl()
     {
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && (characterController.isGrounded || canSecondJump))
         {
-            // TODO: se puede saltar super alto con macros
-            rigidbody.AddForce(Vector3.up * jumpForce);
+            verticalVelocity = jumpForce / 50;
+            if (!characterController.isGrounded) canSecondJump = false;
         }
+        if (characterController.isGrounded) canSecondJump = true;
     }
     void FixedMovementControl()
     {
-        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime;
-        float moveY = Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime;
-        rigidbody.MovePosition(rigidbody.position + transform.TransformDirection(new Vector3(moveX, 0, moveY)));
+        Vector2 rawInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rawInput = Vector2.ClampMagnitude(rawInput, 1f);
+        float moveX = rawInput.x * moveSpeed * Time.fixedDeltaTime;
+        float moveY = rawInput.y * moveSpeed * Time.fixedDeltaTime;
+
+        if (!characterController.isGrounded)
+        {
+            verticalVelocity -= (gravity / 5) * Time.fixedDeltaTime;
+            verticalVelocity = Mathf.Clamp(verticalVelocity, -10, 10);
+        }
+
+        characterController.Move(transform.TransformDirection(new Vector3(moveX, verticalVelocity, moveY)));
     }
 
     void CameraControl()
